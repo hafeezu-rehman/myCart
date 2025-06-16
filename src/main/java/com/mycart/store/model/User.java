de4d32096dd88cc7eb9b8dea9826108c6e27a040
Hafeez Ur Rehman <hafeezurehman7851@gmail.com>
2025-06-16 14:12:50 +0500

    Added User Model without configuring DB

--- src/main/java/com/mycart/store/model/User.java
+++ src/main/java/com/mycart/store/model/User.java
@@ -1,5 +1,66 @@
 package com.mycart.store.model;
 
 public class User {
-
+    private Long user_id;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+    private String phone_number;
+    private String add_id;
+    public User() {
+    }
+    public User(Long user_id, String username, String email, String password, String role, String phone_number,
+            String add_id) {
+        this.user_id = user_id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.phone_number = phone_number;
+        this.add_id = add_id;
+    }
+    public Long getUser_id() {
+        return user_id;
+    }
+    public void setUser_id(Long user_id) {
+        this.user_id = user_id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public String getPhone_number() {
+        return phone_number;
+    }
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+    public String getAdd_id() {
+        return add_id;
+    }
+    public void setAdd_id(String add_id) {
+        this.add_id = add_id;
+    }
+    
 }
