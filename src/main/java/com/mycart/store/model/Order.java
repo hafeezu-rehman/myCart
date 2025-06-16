52548aa8eeac7cacec278173f6b0f7841659fcb3
Hafeez Ur Rehman <hafeezurehman7851@gmail.com>
2025-06-16 17:19:31 +0500

    Order Model with DB configuration

--- src/main/java/com/mycart/store/model/Order.java
+++ src/main/java/com/mycart/store/model/Order.java
@@ -1,19 +1,36 @@
 package com.mycart.store.model;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="ORDER")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ORD_ID")
     private Long ord_id;
+    @Column(name = "Status", nullable = false, unique = false)
     private String status;
+    @Column(name = "TOTAL_AMOUNT", nullable = false, unique = false)
     private double total_amount;
+    @ManyToOne()
+    @JoinColumn(name = "USER_ID", referencedColumnName = "USER_ID")
     private User user;
-    private Address address;
     public Order() {
     }
-    public Order(Long ord_id, String status, double total_amount, User user, Address address) {
+    public Order(Long ord_id, String status, double total_amount, User user) {
         this.ord_id = ord_id;
         this.status = status;
         this.total_amount = total_amount;
         this.user = user;
-        this.address = address;
     }
     public Long getOrd_id() {
         return ord_id;
@@ -39,11 +56,4 @@ public class Order {
     public void setUser(User user) {
         this.user = user;
     }
-    public Address getAddress() {
-        return address;
-    }
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-    
 }
