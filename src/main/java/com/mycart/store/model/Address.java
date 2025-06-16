e65b1876b238343e3c44b2a25e59156c9f943d4a
Hafeez Ur Rehman <hafeezurehman7851@gmail.com>
2025-06-16 17:05:39 +0500

    Address Model with DB configuration

--- src/main/java/com/mycart/store/model/Address.java
+++ src/main/java/com/mycart/store/model/Address.java
@@ -1,18 +1,35 @@
 package com.mycart.store.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "ADDRESS")
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "ADD_ID")
     private Long add_id;
+    @Column(name = "HOUSE", nullable = false, unique = false)
     private String house;
-    private String steet;
+    @Column(name = "STREET", nullable = false, unique = false)
+    private String street;
+    @Column(name = "CITY", nullable = false, unique = false)
     private String city;
+    @Column(name = "PROVINCE", nullable = false, unique = false)
     private String province;
+    @Column(name = "COUNTRY", nullable = false, unique = false)
     private String country;
     public Address() {
     }
-    public Address(Long add_id, String house, String steet, String city, String province, String country) {
+    public Address(Long add_id, String house, String street, String city, String province, String country) {
         this.add_id = add_id;
         this.house = house;
-        this.steet = steet;
+        this.street = street;
         this.city = city;
         this.province = province;
         this.country = country;
@@ -29,11 +46,11 @@ public class Address {
     public void setHouse(String house) {
         this.house = house;
     }
-    public String getSteet() {
-        return steet;
+    public String getStreet() {
+        return street;
     }
-    public void setSteet(String steet) {
-        this.steet = steet;
+    public void setStreet(String street) {
+        this.street = street;
     }
     public String getCity() {
         return city;
