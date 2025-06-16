3829d4f29f4525f34cf5b62a095456d351677f7f
Hafeez Ur Rehman <hafeezurehman7851@gmail.com>
2025-06-16 17:20:09 +0500

    Removed unused library from Order Model

--- src/main/java/com/mycart/store/model/Order.java
+++ src/main/java/com/mycart/store/model/Order.java
@@ -1,6 +1,5 @@
 package com.mycart.store.model;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
