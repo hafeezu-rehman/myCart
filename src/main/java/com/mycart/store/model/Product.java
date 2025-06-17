ca273112cc090591520e141c6855f5bd1a391fdd
Hafeez Ur Rehman <hafeezurehman7851@gmail.com>
2025-06-17 16:20:41 +0500

    Removed unused libraries

--- src/main/java/com/mycart/store/model/Product.java
+++ src/main/java/com/mycart/store/model/Product.java
@@ -7,9 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import java.util.*;
 @Entity
 @Table(name = "PRODUCT")
 public class Product {
